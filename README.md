c94527362ec7fdccd2424fad3c61e3cb0c9a90b8
Vignesh Pethuraj <vignesh@Vigneshs-MacBook-Pro.local>
2021-01-25 21:48:30 -0800

    updated without cronJob

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # UW-class-signup-bot
-A bot that can be used to sign up for University of Washington classes
+A bot that can be ran throughout the day and can be used to automatically sign up for University of Washington classes.
 
 ## Instructions
 1. Make sure to have node installed along with npm.
 2. `cd` to the parent folder of this repo locally and run `npm i` to install all dependencies
-3. In this Node.js script there are two urls you will need to provide: `const classDetailsURL` and `const classSignupURL`. Example URLs are provided so you will know how they look.
-4. In `credentials.json` provde your UW NetID login credentials.
-5. In the `async function startTracking(cookies, browser, classPage)` method you have the ability to change the frequency of the `CronJob`. 
+3. `app.js` has the src code for this bot. Feel free to add or contribute or drop suggestions 
+4. The `.env` file is where you will need to provide two URL links and your UW NetID credentials. Sample data is provided as an example.
+5. In the `async function startTracking(cookies, browser, classPage)` method you have the ability to change the frequency of how often you check for the class availability. 
 6. After these parameters are set, let the script run in the background, and you should have your class signed up for in no time!
 
 
--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const puppeteer = require('puppeteer');
-const credentials = require('./credentials.json')
-const CronJob = require('cron').CronJob;
+require('dotenv').config()
 
-//url to see class details such as the amount of students signed up and available spots
-const classDetailsURL = 'https://sdb.admin.uw.edu/timeschd/uwnetid/sln.asp?QTRYR=AUT+2020&SLN=10227'
+// url to see class details such as the amount of students signed up and available spots
+const classDetailsURL = process.env.CLASS_DETAILS_URL
 
-//url to sign up for the class; once accessed student is auto-signed up.
-const classSignupURL = 'https://sdb.admin.uw.edu/students/UWNetID/register.asp?INPUTFORM=UPDATE&PAC=0&MAXDROPS=0&'
-       +'_CW=5df0ad466c6cdc0cd2f42cbc5323bca84ea345ca1bf55dbb5c75b2f8a26b1bf3&_src=mp_reg_preview&QTR=4&YR=2020&sln1=10227&entCode1=&credits1=&gr_sys1='
+// url to sign up for the class; once accessed student is auto-signed up.
+const classSignupURL = process.env.CLASS_SIGNUP_URL
 
 var cronClose = false;
 
+const startBot = () => {
+    console.log('Welcome to the UW Class Sign-up Bot. Please read the provided')
+    console.log('README.md file for instructions on usage and ensure that you')
+    console.log('have modified the .env file to your class specific details.')
+}
 
 async function browserConfig() {
     const browser = await puppeteer.launch();
     var classPage = await browser.newPage();
+    console.log('hi', classDetailsURL)
     //to get class details
     await classPage.goto(classDetailsURL)
 
     //login
-    await classPage.type('#weblogin_netid', credentials.username)
-    await classPage.type('#weblogin_password', credentials.password)
+    console.log('Configuring the bot to take in your UW credentials...')
+    await classPage.type('#weblogin_netid', process.env.UW_NETID)
+    await classPage.type('#weblogin_password', process.env.UW_PASSWORD)
     await classPage.click('#submit_button')
     await classPage.waitForNavigation()
     console.log('UW NetID login was successful.')
@@ -30,9 +35,9 @@ async function browserConfig() {
 
     //accessing the class information page with the cookies
     classPage = await browser.newPage()
-    await classPage.setCookie(...cookies)
-    await classPage.goto(classDetailsURL)
-
+    // await classPage.setCookie(...cookies)
+    // await classPage.goto(classDetailsURL)
+    console.log('Checking class capacity...')
     return {cookies, browser, classPage};
 }
 
@@ -51,17 +56,17 @@ async function checkClassCapacity(cookies, browser, classPage) {
             capacity = Number(capacity.replace(/[^0-9.-]+/g,""));
 
             let ratio = currEnrollment / capacity;
-
             if (ratio < 1) { //class is not full
                classPage.close();
                const signupPage = browser.newPage();
                await signupPage.setCookie(cookies)
-               signupPage.goto(classSignupURL); //auto signs up when url is accessed;
-               return 'The class you have been waiting for has been signed up for!';
+               await signupPage.goto(classSignupURL); //auto signs up when url is accessed;
                browser.close();
                cronClose = true; //for the cronjob to stop
+               return `${ratio}The class you have been waiting for has been signed up for!`;
+
             } else {
-                return 'The class is still full. Checking again...';
+                return `${ratio}The class is still full. Checking again...`;
             }
 
     })
@@ -70,21 +75,30 @@ async function checkClassCapacity(cookies, browser, classPage) {
 
 
 async function startTracking(cookies, browser, classPage) {
-    let job = new CronJob('*/15 * * * *', function() {
-        checkClassCapacity(cookies, browser, classPage);
-    }, null, true, null, null, true);
 
-    job.start();
+    let intervalTask =  setInterval(() => {
+        checkClassCapacity(cookies, browser, classPage);        
+    }, 15000); // USER PARAM: you can set the time here in ms
+
+
+    // let job = new CronJob('*/15 * * * *', function() {
+    //     checkClassCapacity(cookies, browser, classPage);
+    // }, null, true, null, null, true);
+
+    //job.start();
 
     if (cronClose) {
-        job.stop();
-        console.log('cron job has been stopped')
+        //job.stop();
+        clearInterval(intervalTask)
+        console.log('Checking class capacity task has been terminated.')
     }
 }
 
 async function bootstrap() {
+    startBot()
     let {cookies, browser, classPage} = await browserConfig();
     await startTracking(cookies, browser, classPage);
 }
 
-bootstrap();
+
+bootstrap(); 
